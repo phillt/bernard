0051aba60d24e1b22110829036b93971f3f858d6
Philip <powerphillg5@gmail.com>
2026-02-09 08:17:18 -0800

    Update tool creation to include memory system

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,10 +1,16 @@
 import { createShellTool } from './shell.js';
+import { createMemoryTool, createScratchTool } from './memory.js';
+import { createDateTimeTool } from './datetime.js';
 import type { ToolOptions } from './types.js';
+import type { MemoryStore } from '../memory.js';
 
 export type { ToolOptions } from './types.js';
 
-export function createTools(options: ToolOptions) {
+export function createTools(options: ToolOptions, memoryStore: MemoryStore) {
   return {
     shell: createShellTool(options),
+    memory: createMemoryTool(memoryStore),
+    scratch: createScratchTool(memoryStore),
+    datetime: createDateTimeTool(),
   };
 }
