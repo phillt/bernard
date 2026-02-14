16ebefa11336c6da479e02180c8926bce543be48
Philip <powerphillg5@gmail.com>
2026-02-14 12:45:06 -0800

    Initialize theme system on application startup

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as readline from 'node:readline';
 import { loadConfig, saveProviderKey, removeProviderKey, getProviderKeyStatus, PROVIDER_ENV_VARS, OPTIONS_REGISTRY, resetOption, resetAllOptions } from './config.js';
 import { startRepl } from './repl.js';
 import { printWelcome, printError, printInfo } from './output.js';
+import { setTheme } from './theme.js';
 import { CronStore } from './cron/store.js';
 import { cronList, cronDelete, cronDeleteAll, cronStop, cronBounce } from './cron/cli.js';
 import { listMCPServers, removeMCPServer } from './mcp.js';
@@ -28,6 +29,8 @@ program
         provider: opts.provider,
         model: opts.model,
       });
+
+      setTheme(config.theme);
 
       let alertContext: string | undefined;
 
